2b98e068dfb981b379fe9e82bf08deb0a2cbf562
NicolasPant <nicolas.panteleimonow@justice.fr>
2026-03-20 15:11:37 +0100

    wip: added config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // CRA's default build output
-  },
-  base: '/label',
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'REACT_');
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'build',
     },
-  },
+    base: '/label',
+    server: {
+      port: 55432,
+    },
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      'process.env': env,
+    },
+  };
 });
